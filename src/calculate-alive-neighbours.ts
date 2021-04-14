8b697478a21c7d71b79b623f9879c92c1b089bdf
kbayrhammer <klaus.bayrhammer@redbull.com>
2021-04-14 11:07:24 +0200

    refactor: simplified calculate neighbours

--- src/calculate-alive-neighbours.ts
+++ src/calculate-alive-neighbours.ts
@@ -1,21 +1,17 @@
 import {CellState} from "./game-of-life";
 
-export default function calculateAliveNeighbours(cellX: number, cellY: number, board: CellState[][]): number {
-    const lowerXBound = Math.max(cellX - 1, 0);
-    const upperXBound = Math.min(cellX + 1, board[0].length - 1);
-    const lowerYBound = Math.max(cellY - 1, 0);
-    const upperYBound = Math.min(cellY + 1, board.length - 1);
+export default function calculateAliveNeighbours(x: number, y: number, board: CellState[][]): number {
+    return [
+        board[x-1]?.[y-1],
+        board[x-1]?.[y],
+        board[x-1]?.[y+1],
 
-    const relevantNeighbours = board.flatMap((row, x) =>
-        row.filter((cell, y) => {
-            if (x === cellX && y === cellY) {
-                return false
-            }
-            return lowerXBound <= x && x <= upperXBound &&
-                lowerYBound <= y && y <= upperYBound;
-        })
-    )
-    return relevantNeighbours
-        .filter(cellState => cellState === CellState.Alive)
+        board[x]?.[y-1],
+        board[x]?.[y+1],
+
+        board[x+1]?.[y-1],
+        board[x+1]?.[y],
+        board[x+1]?.[y+1]
+    ].filter(cellState => cellState === CellState.Alive)
         .length
 }
